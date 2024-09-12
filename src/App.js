1775f96a7f495397eed466183c8e813ed70026a7
martinsglucas <martin5.luca5.univ@gmail.com>
2024-09-12 17:29:37 -0300

    adiciona react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Escala from "./pages/Escala";
+import Ministerio from "./pages/Ministerio";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
+import Perfil from "./pages/Perfil";
+import Header from "./components/layout/Header";
+import Indisponibilidade from "./pages/Indisponibilidade";
+import Footer from "./components/layout/Footer";
+import Container from "./components/layout/Container";
+import ProtectedRoute from "./components/layout/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Container customClass="min_height">
+        <Routes>
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/indisponibilidade"
+            element={
+              <ProtectedRoute>
+                <Indisponibilidade />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/escala"
+            element={
+              <ProtectedRoute>
+                <Escala />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ministerio"
+            element={
+              <ProtectedRoute>
+                <Ministerio />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/perfil"
+            element={
+              <ProtectedRoute>
+                <Perfil />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
