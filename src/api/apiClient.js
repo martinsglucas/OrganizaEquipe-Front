6c289cde2f37065eae0af2b8d7cd78ddcaab1c42
martinsglucas <martin5.luca5.univ@gmail.com>
2024-09-12 17:27:37 -0300

    configura requisicoes para api

---
+++ src/api/apiClient.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: "http://127.0.0.1:8000/api/",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
