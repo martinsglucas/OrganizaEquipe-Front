202221079c53f0732d2607c8a956b8aef1db4336
martinsglucas <martin5.luca5.univ@gmail.com>
2024-10-01 18:34:54 -0300

    atualiza Select

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -2,18 +2,18 @@ import styles from "./Select.module.css";
 
 function Select({ text, name, options, handleOnChange, value }) {
   return (
-    <div className={styles.form_control}>
-      <label htmlFor={name}>{text}</label>
+    <div className={styles.container}>
+      {/* <label htmlFor={name}>{text}</label> */}
       <select
         id={name}
         name={name}
         onChange={handleOnChange}
         value={value || ""}
       >
-        <option>Selecione uma opção</option>
+        <option>{text}</option>
         {options.map((option) => (
           <option value={option.id} key={option.id}>
-            {option.name}
+            {option.nome}
           </option>
         ))}
       </select>
