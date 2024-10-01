f1e79f186589d3c43c682f9870aa1cf8338d3e09
martinsglucas <martin5.luca5.univ@gmail.com>
2024-10-01 18:39:45 -0300

    atualiza App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Escala from "./pages/Escala";
-import Ministerio from "./pages/Ministerio";
+import Equipe from "./pages/Equipe";
 import Cadastro from "./pages/Cadastro";
 import Login from "./pages/Login";
 import Perfil from "./pages/Perfil";
@@ -11,59 +12,64 @@ import Indisponibilidade from "./pages/Indisponibilidade";
 import Footer from "./components/layout/Footer";
 import Container from "./components/layout/Container";
 import ProtectedRoute from "./components/layout/ProtectedRoute";
+import { EquipeProvider } from "./context/EquipeContext";
 
 function App() {
+  const [sidebar, setSidebar] = useState(false);
+
   return (
-    <Router>
-      <Header />
-      <Container customClass="min_height">
-        <Routes>
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/indisponibilidade"
-            element={
-              <ProtectedRoute>
-                <Indisponibilidade />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/escala"
-            element={
-              <ProtectedRoute>
-                <Escala />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/ministerio"
-            element={
-              <ProtectedRoute>
-                <Ministerio />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/perfil"
-            element={
-              <ProtectedRoute>
-                <Perfil />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Container>
-      <Footer />
-    </Router>
+    <EquipeProvider>
+      <Router>
+        <Header sidebar={sidebar} setSitebar={setSidebar} />
+        <Container customClass="min_height">
+          <Routes>
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/indisponibilidade"
+              element={
+                <ProtectedRoute>
+                  <Indisponibilidade />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/escala"
+              element={
+                <ProtectedRoute>
+                  <Escala />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/equipe"
+              element={
+                <ProtectedRoute>
+                  <Equipe />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/perfil"
+              element={
+                <ProtectedRoute>
+                  <Perfil />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Container>
+        <Footer setSidebar={setSidebar} />
+      </Router>
+    </EquipeProvider>
   );
 }
 
