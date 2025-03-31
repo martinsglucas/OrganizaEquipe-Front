cc734b5d0ae0ee2365b4d435a10f761f419c89e7
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 11:18:28 -0300

    feat: atualiza página Equipe
    adiciona funcionalidade de criar equipe
    
    #4

--- src/pages/Equipe.js
+++ src/pages/Equipe.js
@@ -3,11 +3,13 @@ import { getTeams, getTeam } from "../api/services/teamService";
 import { useEffect, useState } from "react";
 import EquipeDetalhe from "../components/EquipeDetalhe";
 import { useEquipe } from "../context/EquipeContext";
-// import Select from "../components/form/Select";
+import ModalCreateTeam from "../components/modals/ModalCreateTeam";
+import { createTeam } from "../api/services/teamService";
 
 function Equipe() {
   const [equipes, setEquipes] = useState([]);
   const { equipe, setEquipe } = useEquipe();
+  const [showModal, setShowModal] = useState(false);
 
   const getEquipes = async () => {
     try {
@@ -19,29 +21,27 @@ function Equipe() {
   };
 
   useEffect(() => {
-    getEquipes();
+    if (!equipe){
+      getEquipes();
+    }
   }, []);
 
-  // const handleEquipeChange = async (event) => {
-  //   const selectedEquipe = await getTeam(event.target.value);
-  //   setEquipe(selectedEquipe);
-  // };
 
   const handleSwapTeam = async (id) => {
     const equipe = await getTeam(id);
     setEquipe(equipe);
   };
 
+  const addEquipe = async (nome) => {
+    const equipe = await createTeam({ nome });
+    setEquipes([...equipes, equipe]);
+    setShowModal(false);
+  };
+
   if (!equipe) {
     return (
       <div className={styles.container}>
         <h2>Nenhuma equipe selecionada</h2>
-        {/* <Select
-          text={"Selecione uma equipe"}
-          name={"select"}
-          options={equipes}
-          handleOnChange={handleEquipeChange}
-        /> */}
         <div className={styles.equipes}>
           {equipes.map((equipe) => (
             <button
@@ -52,7 +52,16 @@ function Equipe() {
               {equipe.nome}
             </button>
           ))}
+          <button className={styles.add} onClick={() => setShowModal(true)}>
+            <span>+</span>
+          </button>
         </div>
+        {showModal && (
+          <ModalCreateTeam
+            closeModal={() => setShowModal(false)}
+            handleCreateTeam={addEquipe}
+          />
+        )}
       </div>
     );
   }
@@ -60,7 +69,7 @@ function Equipe() {
   return (
     <div className={styles.container}>
       <div className={styles.equipes}></div>
-      <EquipeDetalhe equipe={equipe} equipes={equipes} />
+      <EquipeDetalhe equipes={equipes} setEquipes={setEquipes}/>
     </div>
   );
 }
