89afef6365e4df5bed9feb8b4ae0f2fcbc6d12f0
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 11:11:36 -0300

    feat: utiliza useAuth no ProtectedRoute
    #3

--- src/components/layout/ProtectedRoute.js
+++ src/components/layout/ProtectedRoute.js
@@ -1,10 +1,24 @@
 import styles from "./ProtectedRoute.module.css";
 import LinkButton from "../LinkButton";
+import { useAuth } from "../../context/AuthContext";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { logoutUser } from "../../api/services/userService";
 
 function ProtectedRoute({ children }) {
-  const isAuthenticated = !!localStorage.getItem("accessToken");
 
-  return isAuthenticated ? (
+  const { isSignedIn, isLoading } = useAuth();
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (!isLoading) {
+  //     if (!isSignedIn) {
+  //       navigate("/login");
+  //     }
+  //   }
+  // }, [isSignedIn, isLoading]);
+
+  return isSignedIn ? (
     children
   ) : (
     <div className={styles.container}>
