806af88d5edc2676bbc816d82c77d955c4262ed5
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 12:02:51 -0300

    feat: adiciona elementos na página Indisponibilidade

--- src/pages/Indisponibilidade.js
+++ src/pages/Indisponibilidade.js
@@ -1,8 +1,73 @@
+import styles from "./Indisponibilidade.module.css";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { useState, useEffect } from "react";
+import dayjs from "dayjs";
+import Calendar from "../components/Calendar";
+import ModalCreateUnavailability from "../components/modals/ModalCreateUnavailability";
+import { getUnavailabilities } from "../api/services/unavailabilityService";
+import IndisponibilidadeDetalhe from "../components/IndisponibilidadeDetalhe";
+import { toast } from "react-toastify";
+
 function Indisponibilidade() {
+  const [value, setValue] = useState(dayjs());
+  const [showModal, setShowModal] = useState(false);
+  const [indisponibilidades, setIndisponibilidades] = useState([]);
+  const [currentMonth, setCurrentMonth] = useState(dayjs(value).month());
+
+  const getIndisponibilidades = async (date) => {
+    try {
+      const response = await getUnavailabilities(
+        true,
+        dayjs(date).format("YYYY-MM-DD")
+      );
+      setIndisponibilidades(response);
+    } catch (error) {
+      console.error("Erro ao buscar indisponibilidades:", error);
+      toast.error("Erro ao buscar indisponibilidades");
+    }
+  };
+
+  useEffect(() => {
+    const newMonth = dayjs(value).month();
+    if (newMonth !== currentMonth) {
+      setCurrentMonth(newMonth);
+      getIndisponibilidades(value);
+    }
+  }, [value, currentMonth]);
+
+  const handleDateChange = (newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div>
-      <h1>Indisponibilidade</h1>
-      <p>Welcome to the Indisponibilidade page!</p>
+    <div className={styles.container}>
+      <AiFillPlusCircle
+        className={styles.add}
+        onClick={() => setShowModal(true)}
+      />
+      <Calendar
+        onChange={handleDateChange}
+        refreshData={getIndisponibilidades}
+        data={indisponibilidades}
+        value={value}
+      />
+      {value && (
+        <div className={styles.indisponibilidades}>
+          <IndisponibilidadeDetalhe
+            indisponibilidades={indisponibilidades}
+            setIndisponibilidades={setIndisponibilidades}
+            dia={value}
+          />
+        </div>
+      )}
+      {showModal && (
+        <ModalCreateUnavailability
+          onClose={() => setShowModal(false)}
+          day={value}
+          unavailabilities={indisponibilidades}
+          setUnavailabilities={setIndisponibilidades}
+        />
+      )}
     </div>
   );
 }
