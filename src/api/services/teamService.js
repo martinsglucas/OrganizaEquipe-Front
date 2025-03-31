123937e049f6c6fbfbc12accb84ce66c5deb8b1b
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 11:27:29 -0300

    feat: adiciona outros métodos da Equipe
    #4

--- src/api/services/teamService.js
+++ src/api/services/teamService.js
@@ -30,3 +30,49 @@ export const createTeam = async (teamData) => {
     throw error;
   }
 };
+
+export const updateTeam = async (id, teamData) => {
+  try {
+    const response = await apiClient.patch(`equipes/${id}/`, teamData);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao atualizar equipe:", error);
+    throw error;
+  }
+};
+
+export const deleteTeam = async (id) => {
+  try {
+    const response = await apiClient.delete(`equipes/${id}/`);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao deletar equipe:", error);
+    throw error;
+  }
+};
+
+export const addMember = async (id, memberId) => {
+  try {
+    const response = await apiClient.post(
+      `equipes/${id}/add_member/`,
+      memberId
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao adicionar membro:", error);
+    throw error;
+  }
+};
+
+export const removeMember = async (id, memberId) => {
+  try {
+    const response = await apiClient.post(
+      `equipes/${id}/remove_member/`,
+      memberId
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao remover membro:", error);
+    throw error;
+  }
+};
