9514b44a4a3cb9e7a4fc1069916cc54b0781afbd
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 12:02:15 -0300

    feat: cria componente Calendar
    #7

---
+++ src/components/Calendar.js
@@ -0,0 +1,92 @@
+import { DateCalendar } from "@mui/x-date-pickers";
+import { useState, useEffect } from "react";
+import "./Calendar.css";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { Badge } from "@mui/material";
+import { PickersDay } from "@mui/x-date-pickers";
+import { DayCalendarSkeleton } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+import "dayjs/locale/pt-br";
+dayjs.locale("pt-br");
+
+function Calendar({ onChange, refreshData, data, value }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [highlightedDays, setHighlightedDays] = useState([]);
+
+  useEffect(() => {
+    refreshData(value);
+  }, []);
+
+  function ServerDay(props) {
+    const { highlightedDays = [], day, outsideCurrentMonth, ...other } = props;
+
+    const isSelected =
+      !props.outsideCurrentMonth &&
+      highlightedDays.indexOf(props.day.date()) >= 0;
+
+    return (
+      <Badge
+        key={props.day.toString()}
+        overlap="circular"
+        badgeContent={isSelected ? "🔴" : undefined}
+      >
+        <PickersDay
+          {...other}
+          outsideCurrentMonth={outsideCurrentMonth}
+          day={day}
+        />
+      </Badge>
+    );
+  }
+
+  const fetchHighlightedDays = () => {
+    const dias = data.map((indisponibilidade) =>
+      dayjs(indisponibilidade.data_inicio).date()
+    );
+    setHighlightedDays(dias);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchHighlightedDays();
+  }, [data]);
+
+  const handleMonthChange = async (date) => {
+    setIsLoading(true);
+    setHighlightedDays([]);
+    await refreshData(date);
+    fetchHighlightedDays(date);
+    setIsLoading(false);
+  };
+
+  const handleDateChange = (newValue) => {
+    // onChange(dayjs(newValue).format("YYYY-MM-DD"));
+    onChange(dayjs(newValue));
+  };
+
+  return (
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+        <DateCalendar
+          locale={"pt-br"}
+          onChange={handleDateChange}
+          views={["year", "month", "day"]}
+          value={value}
+          loading={isLoading}
+          onMonthChange={handleMonthChange}
+          renderLoading={() => <DayCalendarSkeleton />}
+          slots={{ day: ServerDay }}
+          slotProps={{
+            previousIconButton: { title: "Mês anterior" },
+            nextIconButton: { title: "Próximo mês" },
+            day: { highlightedDays },
+          }}
+          showDaysOutsideCurrentMonth
+        />
+      </LocalizationProvider>
+    </div>
+  );
+}
+
+export default Calendar;
