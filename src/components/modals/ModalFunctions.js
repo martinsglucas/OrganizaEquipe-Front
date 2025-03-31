dffff2a030d05fecda767d517077c38ac6fb0676
martinsglucas <martin5.luca5.univ@gmail.com>
2025-03-31 11:24:49 -0300

    feat: cria modal para gerenciar funções
    #4, #5

---
+++ src/components/modals/ModalFunctions.js
@@ -0,0 +1,97 @@
+import Modal from "./Modal";
+import styles from "./ModalFunctions.module.css";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import { MdEdit } from "react-icons/md";
+import { deleteFunction } from "../../api/services/functionService";
+import { toast } from "react-toastify";
+import { useEquipe } from "../../context/EquipeContext";
+import ModalConfirmation from "./ModalConfirmation";
+import ModalCreateOrEditFunction from "./ModalCreateOrEditFunction";
+import { useState } from "react";
+
+function ModalFunctions({ onClose, functions }) {
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [showCreateFunction, setShowCreateFunction] = useState(false);
+  const [functionToDelete, setFunctionToDelete] = useState(null);
+  const [functionToEdit, setFunctionToEdit] = useState(null);
+
+  const { equipe, setEquipe } = useEquipe();
+
+  const handleDeleteFunction = (func) => {
+    setFunctionToDelete(func);
+    setShowConfirmation(true);
+  };
+
+  const handleEditFunction = (func) => {
+    setFunctionToEdit(func);
+    setShowCreateFunction(true);
+  };
+
+  const removeFunction = async () => {
+    try {
+      const newFunctions = functions.filter(
+        (func) => func.id !== functionToDelete.id
+      );
+      await deleteFunction(functionToDelete.id);
+      setEquipe({ ...equipe, funcoes: newFunctions });
+      toast.success("Função removida com sucesso!");
+      onClose();
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao remover função!");
+    }
+  };
+
+  return (
+    <Modal isOpen={true} onClose={onClose} title={"Funções"}>
+      <div className={styles.functions}>
+        {functions.map((func) => (
+          <div className={styles.func} key={func.id}>
+            <span>{func.nome}</span>
+            <div className={styles.buttons}>
+              <MdEdit
+                onClick={() => {
+                  handleEditFunction(func);
+                }}
+                className={styles.edit}
+              />
+              <FaTrash
+                onClick={() => {
+                  handleDeleteFunction(func);
+                }}
+                className={styles.delete}
+              />
+            </div>
+          </div>
+        ))}
+        <button
+          onClick={() => setShowCreateFunction(true)}
+          className={styles.add}
+        >
+          <FaPlus />
+        </button>
+      </div>
+      {showConfirmation && (
+        <ModalConfirmation
+          title={"Excluir função"}
+          message={`Tem certeza que deseja remover a função ${functionToDelete.nome}?`}
+          onConfirm={() => removeFunction()}
+          onClose={() => setShowConfirmation(false)}
+          noMarginTop={true}
+        />
+      )}
+      {showCreateFunction && (
+        <ModalCreateOrEditFunction
+          title={"Criar função"}
+          func={functionToEdit}
+          onClose={() => {
+            setShowCreateFunction(false);
+            setFunctionToEdit(null);
+          }}
+        />
+      )}
+    </Modal>
+  );
+}
+
+export default ModalFunctions;
