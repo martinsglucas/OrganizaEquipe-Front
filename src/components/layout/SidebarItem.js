7cb3d72c5f860c878a5e31f719f715dcc7859cb0
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 16:01:47 -0300

    feat: adiciona opção de highlight
    #11

--- src/components/layout/SidebarItem.js
+++ src/components/layout/SidebarItem.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import styles from "./SidebarItem.module.css";
 
-function SidebarItem({ Icon, Text, to, onClick }) {
+function SidebarItem({ Icon, Text, to, onClick, highlight }) {
   return (
-    <Link to={to} className={styles.container} onClick={onClick}>
-      <Icon className={styles.svg} />
+    <Link
+      to={to}
+      className={styles.container}
+      onClick={onClick}
+    >
+      <Icon
+        className={`${styles.svg} ${highlight ? styles.highlight : ""}`}
+      />
       {Text}
     </Link>
   );
