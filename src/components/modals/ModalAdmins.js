ba8bf0dce2fc721e2ad24b7b3905e57910489e31
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 15:24:11 -0300

    style: ajusta estilo ModalAdmins
    #4

--- src/components/modals/ModalAdmins.js
+++ src/components/modals/ModalAdmins.js
@@ -1,20 +1,20 @@
-import styles from "./ModalAdministradores.module.css";
+import styles from "./ModalAdmins.module.css";
 import SelectCheckbox from "../form/SelectCheckbox";
-import { useEquipe } from "../../context/EquipeContext";
+import { useTeam } from "../../context/TeamContext";
 import { useState, useEffect } from "react";
 import Modal from "./Modal";
 import { updateTeam } from "../../api/services/teamService";
 import { toast } from "react-toastify";
-function ModalAdministradores({ isOpen, onClose }) {
-  const { equipe, setEquipe } = useEquipe();
-  const membros = equipe?.membros || [];
-  const [administradores, setAdministradores] = useState(equipe?.administradores || []);
+function ModalAdmins({ isOpen, onClose }) {
+  const { equipe, setEquipe } = useTeam();
+  const membros = equipe?.members || [];
+  const [administradores, setAdministradores] = useState(equipe?.admins || []);
   const [disabled, setDisabled] = useState(true);
 
 
   useEffect(() => {
     const administradoresIds = administradores.map((admin) => admin.id);
-    const equipeAdministradoresIds = (equipe?.administradores || []).map(
+    const equipeAdministradoresIds = (equipe?.admins || []).map(
       (admin) => admin.id
     );
 
@@ -30,19 +30,19 @@ function ModalAdministradores({ isOpen, onClose }) {
       toast.warn("A equipe deve ter ao menos um administrador!")
     }
 
-  }, [administradores, equipe?.administradores]);
+  }, [administradores, equipe?.admins]);
 
 
   const updateAdministradores = async () => {
     try {
       const administradoresIds = administradores.map((admin) => admin.id);
-      await updateTeam(equipe.id, { administradores: administradoresIds });
+      await updateTeam(equipe.id, { admins: administradoresIds });
       toast.success("Administradores atualizados com sucesso!");
     } catch (error) {
       console.error("Erro ao atualizar administradores:", error);
       toast.error("Erro ao atualizar administradores");
     } finally {
-      setEquipe({ ...equipe, administradores: administradores });
+      setEquipe({ ...equipe, admins: administradores });
       onClose();
     }
   }
@@ -77,4 +77,4 @@ function ModalAdministradores({ isOpen, onClose }) {
   );
 }
 
-export default ModalAdministradores;
+export default ModalAdmins;
