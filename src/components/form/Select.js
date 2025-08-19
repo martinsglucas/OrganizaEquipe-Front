cbdfde337fda4e327d0f4a36a9d1f12946e6a54c
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 15:36:05 -0300

    refactor: refatora Select
    #1

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -3,17 +3,18 @@ import styles from "./Select.module.css";
 function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.container}>
-      {/* <label htmlFor={name}>{text}</label> */}
+      <label htmlFor={name}>{text}</label>
       <select
         id={name}
         name={name}
         onChange={handleOnChange}
         value={value || ""}
       >
-        <option>{text}</option>
+        <option value={""} disabled>{`Selecione ${text.toLowerCase()}`}</option>
+
         {options.map((option) => (
           <option value={option.id} key={option.id}>
-            {option.nome}
+            {option.name}
           </option>
         ))}
       </select>
