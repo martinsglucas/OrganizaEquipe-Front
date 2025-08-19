ff6a0e6fafad1d78b8df2c734a95e2d1dcbeb82a
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 16:06:47 -0300

    feat: atualiza imports e componentes
    #11

--- src/App.js
+++ src/App.js
@@ -2,109 +2,112 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Home from "./pages/Home";
-import Escala from "./pages/Escala";
-import Equipe from "./pages/Equipe";
-import Cadastro from "./pages/Cadastro";
+import Schedule from "./pages/Schedule";
+import Team from "./pages/Team";
+import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Perfil from "./pages/Perfil";
+import Profile from "./pages/Profile";
 import Header from "./components/layout/Header";
-import Indisponibilidade from "./pages/Indisponibilidade";
-import Organizacao from "./pages/Organizacao";
+import Unavailability from "./pages/Unavailability";
+import Organization from "./pages/Organization";
 import Footer from "./components/layout/Footer";
 import Container from "./components/layout/Container";
 import ProtectedRoute from "./components/layout/ProtectedRoute";
-import { EquipeProvider } from "./context/EquipeContext";
+import { EquipeProvider } from "./context/TeamContext";
 import { AuthProvider } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Notificacoes from "./pages/Notificacoes";
+import Notifications from "./pages/Notifications";
+import { OrganizationProvider} from "./context/OrganizationContext";
 
 function App() {
   const [sidebar, setSidebar] = useState(false);
 
   return (
     <AuthProvider>
-      <EquipeProvider>
-        <Router>
-          <Header sidebar={sidebar} setSitebar={setSidebar} />
-          <Container customClass="min_height">
-            <Routes>
-              <Route path="/cadastro" element={<Cadastro />} />
-              <Route path="/login" element={<Login />} />
-              <Route
-                path="/"
-                element={
-                  <ProtectedRoute>
-                    <Home />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/indisponibilidade"
-                element={
-                  <ProtectedRoute>
-                    <Indisponibilidade />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/escala"
-                element={
-                  <ProtectedRoute>
-                    <Escala />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/equipe"
-                element={
-                  <ProtectedRoute>
-                    <Equipe />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/organizacao"
-                element={
-                  <ProtectedRoute>
-                    <Organizacao />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/perfil"
-                element={
-                  <ProtectedRoute>
-                    <Perfil />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/notificacoes"
-                element={
-                  <ProtectedRoute>
-                    <Notificacoes />
-                  </ProtectedRoute>
-                }
-              />
-            </Routes>
-          </Container>
-          <Footer setSidebar={setSidebar} />
-          <ToastContainer
-            position="top-center"
-            autoClose={2000}
-            hideProgressBar
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-            theme="colored"
-            // theme="dark"
-          />
-        </Router>
-      </EquipeProvider>
+      <OrganizationProvider>
+        <EquipeProvider>
+          <Router>
+            <Header sidebar={sidebar} setSitebar={setSidebar} />
+            <Container customClass="min_height">
+              <Routes>
+                <Route path="/cadastro" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      <Home />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/indisponibilidade"
+                  element={
+                    <ProtectedRoute>
+                      <Unavailability />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/escala"
+                  element={
+                    <ProtectedRoute>
+                      <Schedule />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/equipe"
+                  element={
+                    <ProtectedRoute>
+                      <Team />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/organizacao"
+                  element={
+                    <ProtectedRoute>
+                      <Organization />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/perfil"
+                  element={
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/notificacoes"
+                  element={
+                    <ProtectedRoute>
+                      <Notifications />
+                    </ProtectedRoute>
+                  }
+                />
+              </Routes>
+            </Container>
+            <Footer setSidebar={setSidebar} />
+            <ToastContainer
+              position="top-center"
+              autoClose={2000}
+              hideProgressBar
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
+              theme="colored"
+              // theme="dark"
+            />
+          </Router>
+        </EquipeProvider>
+      </OrganizationProvider>
     </AuthProvider>
   );
 }
