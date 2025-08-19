55a3aa51a5aef8151e27549eab0e6140c310126c
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 16:02:53 -0300

    feat: adiciona opção de logout e busca por notificações
    #11

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,11 +1,48 @@
 import styles from "./Sidebar.module.css";
 import { FaTimes } from "react-icons/fa";
-import { MdPerson, MdPersonOff, MdNotifications } from "react-icons/md";
+import { logoutUser } from "../../api/services/userService";
+import { useAuth } from "../../context/AuthContext";
+import { useOrganization } from "../../context/OrganizationContext";
+import { useTeam } from "../../context/TeamContext";
+import { MdPerson, MdPersonOff, MdNotifications, MdNotificationsActive, MdLogout } from "react-icons/md";
 
 import SidebarItem from "./SidebarItem";
+import { getTeamInvitations } from "../../api/services/teamInvitationService";
+import { getOrganizationInvitations } from "../../api/services/organizationInvitationService";
+import { useEffect, useState } from "react";
 
-function Sidebar({ active }) {
+function Sidebar({ active }) { 
+  
   const closeSidebar = () => active(false);
+  
+  const { setEquipe } = useTeam();
+  const { setOrganization } = useOrganization();
+  const [hasNotifications, setHasNotifications] = useState(false);
+  
+  const { user, setUser } = useAuth();
+
+  const getNotifications = async () => {
+    const teamNotifications = await getTeamInvitations(user.email);
+    const orgNotifications = await getOrganizationInvitations(user.email);
+    setHasNotifications(teamNotifications.length > 0 || orgNotifications.length > 0)
+  };
+
+  useEffect(() => {
+    getNotifications();
+  }, []);
+  
+  
+  const logout = () => {
+    logoutUser();
+    setUser(null);
+    setEquipe(null);
+    setOrganization(null);
+    closeSidebar();
+  };
+
+  if (!user) {
+    return null;
+  }
 
   return (
     <div className={styles.container}>
@@ -25,9 +62,16 @@ function Sidebar({ active }) {
         />
         <SidebarItem
           to={"/notificacoes"}
-          Icon={MdNotifications}
+          Icon={hasNotifications ? MdNotificationsActive : MdNotifications}
           Text={"Notificações"}
           onClick={closeSidebar}
+          highlight={hasNotifications}
+        />
+        <SidebarItem
+          to={"/login"}
+          Icon={MdLogout}
+          Text={"Sair"}
+          onClick={logout}
         />
       </div>
     </div>
