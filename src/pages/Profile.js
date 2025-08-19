22849a0ac1c460bc2fd7d3abd4defc6ae3f4de26
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 14:54:15 -0300

    style: reescreve profile
    #3

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,11 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { logoutUser } from "../api/services/userService";
 import { useState } from "react";
 import UserData from "../components/UserData";
-import styles from "./Perfil.module.css";
+import styles from "./Profile.module.css";
 import { useAuth } from "../context/AuthContext";
+import { useTeam } from "../context/TeamContext";
+import { useOrganization } from "../context/OrganizationContext";
 
-function Perfil() {
+function Profile() {
   const [edit, setEdit] = useState(false);
+  const { setEquipe } = useTeam()
+  const { setOrganization } = useOrganization();
 
   const { setUser } = useAuth();
 
@@ -15,6 +19,8 @@ function Perfil() {
   const logout = () => {
     logoutUser();
     setUser(null);
+    setEquipe(null);
+    setOrganization(null);
     navigate("/login");
   };
 
@@ -44,4 +50,4 @@ function Perfil() {
   );
 }
 
-export default Perfil;
+export default Profile;
