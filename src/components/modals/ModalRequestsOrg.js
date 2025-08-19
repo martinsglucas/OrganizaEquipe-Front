8bd57c25db15ff6712070612ca2733e4e6f7c75e
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 15:10:40 -0300

    feat: add ModalRequestsOrg
    #8

---
+++ src/components/modals/ModalRequestsOrg.js
@@ -0,0 +1,81 @@
+import styles from "./ModalRequestsOrg.module.css";
+import Modal from "./Modal";
+import { useState, useEffect } from "react";
+import { deleteRequest, getRequests } from "../../api/services/requestService";
+import { MdCancel, MdDone } from "react-icons/md";
+import { toast } from "react-toastify";
+import { addMember } from "../../api/services/organizationService";
+import { useOrganization } from "../../context/OrganizationContext";
+
+function ModalRequestsOrg({ onClose }) {
+  const { organization, setOrganization } = useOrganization();
+  const [requests, setRequests] = useState([]);
+
+  useEffect(() => {
+    const fetchRequests = async () => {
+      if (organization?.code_access) {
+        try {
+          const response = await getRequests(organization.code_access);
+          setRequests(response);
+        } catch (error) {
+          console.error("Erro ao buscar solicitações:", error);
+        }
+      }
+    };
+
+    fetchRequests();
+  }, [organization]);
+
+  const refuseRequest = async (id) => {
+    try {
+      await deleteRequest(id);
+      const newRequests = requests.filter((request) => request.id !== id);
+      setRequests(newRequests);
+      toast.success("Solicitação recusada com sucesso");
+    } catch (error) {
+      toast.error("Erro ao recusar solicitação");
+    }
+  };
+
+  const acceptRequest = async (request) => {
+    try {
+      await addMember(organization.id, { user_id: request.user.id });
+      setOrganization({ ...organization, members: [...organization.members, request.user] });
+      await deleteRequest(request.id);
+      const newRequests = requests.filter((r) => r.id !== request.id);
+      setRequests(newRequests);
+      toast.success("Solicitação aceita com sucesso");
+    } catch (error) {
+      toast.error("Erro ao aceitar solicitação");
+    }
+  };
+
+  if (!requests || requests.length === 0)
+    return (
+      <Modal isOpen={true} onClose={onClose} title={"Solicitações"}>
+        <p>Sem solicitações</p>
+      </Modal>
+    );
+
+  return (
+    <Modal isOpen={true} onClose={onClose} title={"Solicitações"}>
+      {requests.map((request) => (
+        <div key={request.id} className={styles.request}>
+          <span>{request.user.first_name}</span>
+          <div className={styles.buttons}>
+            <MdCancel
+              className={styles.cancel}
+              onClick={() => refuseRequest(request.id)}
+            />
+            <MdDone
+              className={styles.approve}
+              onClick={() => acceptRequest(request)}
+            />
+          </div>
+        </div>
+      ))}
+    </Modal>
+  );
+}
+
+export default ModalRequestsOrg;
