d86b8fd3257782e5e5ff15204105eee8bb464f0c
martinsglucas <martin5.luca5.univ@gmail.com>
2025-08-19 23:43:02 -0300

    feat: busca membros disponíveis
    #6

--- src/components/modals/ModalAddParticipation.js
+++ src/components/modals/ModalAddParticipation.js
@@ -1,25 +1,45 @@
 import styles from "./ModalAddParticipation.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Select from "../form/Select";
 import SelectCheckbox from "../form/SelectCheckbox";
+import { toast } from "react-toastify";
+import { getAvailableMembers } from "../../api/services/teamService";
 
 function ModalAddParticipation({ title, schedule, participation, participations, setParticipations, onClose }) {
 
   const team = schedule.team;
   const [selectedMember, setSelectedMember] = useState(participation?.user.id || "");
   const [selectedRoles, setSelectedRoles] = useState(participation?.roles || []);
+  const [availableMembers, setAvailableMembers] = useState([])
+  const [unavailableMembers, setUnvailableMembers] = useState([])
+
+
+  const fetchAvailableMembers = async () => {
+    try {
+      const members = await getAvailableMembers(team.id, {date: schedule.date});
+      console.log(members.available_members);
+      setAvailableMembers(members.available_members);
+      setUnvailableMembers(members.unavailable_members.map((m) => ({id: m.id, name:`${m.first_name} (Indisponível)`})));
+    } catch (error) {
+      toast.error("Erro ao buscar membros disponíveis");
+    }
+  }
+
+  useEffect(() => {
+    fetchAvailableMembers();
+  }, []);
 
   const filterMembers = () => {
     if (participation) {
       return team.members
         .map((m) => ({ id: m.id, name: m.first_name }))
         .sort((m1, m2) => m1.name.localeCompare(m2.name));
-    } else {
-      return team.members
-        .map((m) => ({ id: m.id, name: m.first_name }))
-        .filter((m) => !participations.find((p) => p.user.id === m.id))
-        .sort((m1, m2) => m1.name.localeCompare(m2.name));
+      } else {
+        return availableMembers
+          .map((m) => ({ id: m.id, name: m.first_name }))
+          .filter((m) => !participations.find((p) => p.user.id === m.id))
+          .sort((m1, m2) => m1.name.localeCompare(m2.name));
     }
   }
 
@@ -27,6 +47,14 @@ function ModalAddParticipation({ title, schedule, participation, participations,
   const roles = team.roles;
 
   const addParticipation = () => {
+    if (!selectedMember) {
+      toast.warn("Selecione um membro");
+      return;
+    }
+    if (selectedRoles.length <= 0) {
+      toast.warn("Selecione ao menos uma função");
+      return;
+    }
     if (participation) {
       const newParticipations = participations.filter((p) => p.user.id !== participation.user.id);
       setParticipations([
@@ -58,6 +86,7 @@ function ModalAddParticipation({ title, schedule, participation, participations,
             text="Membro"
             name="member"
             options={members}
+            disabledOptions={unavailableMembers}
             value={selectedMember}
             handleOnChange={(e) => {
               setSelectedMember(e.target.value);
