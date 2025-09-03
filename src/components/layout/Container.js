dff6734bddad625fbef61a565020bec2b1d70f71
martinsglucas <martin5.luca5.univ@gmail.com>
2025-09-02 21:05:43 -0300

    feat: adiciona loading ao container
    #11

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -2,22 +2,33 @@ import styles from "./Container.module.css";
 import { useOrganization } from "../../context/OrganizationContext";
 import { getOrganizations } from "../../api/services/organizationService";
 import { getTeams } from "../../api/services/teamService";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
 import { useTeam } from "../../context/TeamContext";
+import { toast } from "react-toastify";
+import ModalLoading from "../modals/ModalLoading";
 
 function Container(props) {
 
   const { setOrganization } = useOrganization();
   const { setTeams } = useTeam();
   const { user } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
   
-    const fillProviders = async () => {
+  const fillProviders = async () => {
+    try {
+      setIsLoading(true);
       const organizations = await getOrganizations(true);
       setOrganization(organizations[0]);
       const teams = await getTeams(true);
       setTeams(teams);
+    } catch (error) {
+      console.error("Erro ao preencher providers:", error);
+      toast.error("Erro ao buscar organização e equipes");
+    } finally {
+      setIsLoading(false);
     }
+  }
 
   useEffect(() => {
     if (user) {
@@ -28,6 +39,7 @@ function Container(props) {
   return (
     <div className={`${styles.container} ${styles[props.customClass]}`}>
       {props.children}
+      {isLoading && (<ModalLoading isOpen={isLoading}/>)}
     </div>
   );
 }
